0ae895d3f95e9314b2fd39085e3ec62f8bf23a83
Beatriz <BEATRIZ.CRUZ84@ETEC.SP.GOV.BR>
2024-04-24 12:29:22 +0000

    SAVEEE

--- app/src/main/java/application/model/Genero.java
+++ app/src/main/java/application/model/Genero.java
@@ -1,5 +1,8 @@
 package application.model;
 
+import jakarta.annotation.Generated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistenco.Entity;
 import jakarta.persistenco.Id;
 import jakarta.persistenco.Table;
@@ -8,6 +11,7 @@ import jakarta.persistenco.Table;
 @Table (name = "generos")
 public class Genero {
     @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
      private long id ; 
      private String nome ;
      public long getID() {
