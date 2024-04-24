0702794ff8d4926ec9b2e4eebc4251582d87c5a7
Beatriz <BEATRIZ.CRUZ84@ETEC.SP.GOV.BR>
2024-04-24 12:24:08 +0000

    save1

--- app/src/main/java/application/model/Genero.java
+++ app/src/main/java/application/model/Genero.java
@@ -1,6 +1,16 @@
 package application.model;
 
+import jakarta.persistenco.Entity;
+
+@Entity
 public class Genero {
+    @Id
      private long id ; 
      private String nome ;
+     public long getID() {
+        return id;
+     }
+     public void setId(long id) {
+        this.id = id;
+     }
 }
